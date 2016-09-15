70ad11633bc005066d860850b1e159887159a08a
Ricard Hagerman <Ricard@Ricards-MacBook-Pro.local>
2016-09-15 09:12:44 +0200

    hey

---
+++ Assets/RPB.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RPB : MonoBehaviour
+{
+
+    public Transform loadingBar;
+    public Transform textIndicator;
+    public Transform textLoading;
+
+    [SerializeField]
+    float currentAmaount;
+    [SerializeField]
+    float speed;
+
+
+    void Start()
+    {
+        textLoading.gameObject.SetActive(false);
+    }
+
+
+    void Update()
+    {
+        if (currentAmaount < 100)
+        {
+            currentAmaount += speed * Time.deltaTime;
+            textIndicator.GetComponent<Text>().text = "" + (int)currentAmaount + " %";
+            textLoading.gameObject.SetActive(true);
+        }
+        else
+        {
+            textLoading.gameObject.SetActive(false);
+            textIndicator.GetComponent<Text>().text = "Done";
+        }
+        loadingBar.GetComponent<Image>().fillAmount = currentAmaount / 100;
+    }
+}
