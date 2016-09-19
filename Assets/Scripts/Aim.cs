a8aaa29c53d4da58925b2ebc40a1af91d5fc5f51
Ricard Hagerman <Ricard@Ricards-MacBook-Pro.local>
2016-09-19 14:27:48 +0200

    improvedBounce

--- Assets/Scripts/Aim.cs
+++ Assets/Scripts/Aim.cs
@@ -16,14 +16,11 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 	GameObject quater;
 	Vector2 aimStartPosition;
 
-	//float startTime;
-	//float endTime;
-
 	public Vector3 quaterPosition;
 	public Level currentLevel;
 	List<GameObject> quatersInPlay;
-
 	float bouncePower;
+	float bounceDamper;
 
 	void Awake ()
 	{
@@ -37,7 +34,7 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 
 	void Start ()
 	{
-		bouncePower = (Screen.height / 10);
+		bounceDamper = (Screen.height / 15);
 		loadingBar.GetComponent<Image> ().fillAmount = 0;
 	}
 
@@ -49,8 +46,10 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 
 	void NewQuater ()
 	{
+		bouncePower = 0;
 		quater = Instantiate (quaterPrefab, Vector3.zero, Quaternion.identity) as GameObject;
 		quatersInPlay.Add (quater);
+		quater.GetComponent<Quater> ().x = 0;
 		quater.GetComponent<Rigidbody> ().isKinematic = true;
 		switch (currentLevel)
 		{
@@ -58,13 +57,13 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 			quaterPosition = new Vector3 (0f, 3.7f, -8f);
 			break;
 		case Level.BEGINNER:	
-			quaterPosition = new Vector3 (0, 4f, -6.3f);
+			quaterPosition = new Vector3 (0, 4f, -6.2f);
 			break;
 		case Level.NORMAL:
-			quaterPosition = new Vector3 (0, 4f, Random.Range (-4.5f, -6.5f));
+			quaterPosition = new Vector3 (0, 4f, Random.Range (-4f, -6.5f));
 			break;
 		case Level.HARD:
-			quaterPosition = new Vector3 (Random.Range (-1f, 1f), 2.5f, Random.Range (-4.5f, -6.5f));
+			quaterPosition = new Vector3 (Random.Range (-1f, 1f), 4f, Random.Range (-4.5f, -6.5f));
 			break;
 		}
 		quater.transform.position = quaterPosition;
@@ -94,7 +93,6 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 
 	public void OnBeginDrag (PointerEventData eventData)
 	{
-		//startTime = Time.time;
 		aimStartPosition = eventData.position;
 	}
 
@@ -105,19 +103,15 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 
 	public void OnEndDrag (PointerEventData eventData)
 	{
-		//endTime = Time.time;
 		if (currentLevel == Level.HARD)
 			quater.GetComponent<Quater> ().x = PowerX (eventData.position);
 		else
 			quater.GetComponent<Quater> ().x = 0;
-		var y = PowerY (eventData.position);
-		quater.transform.rotation = Quaternion.Euler (190, 0, 180);
+		quater.transform.rotation = Quaternion.Euler (200, 0, 180);
 		quater.GetComponent<Rigidbody> ().isKinematic = false;
-		quater.GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
 		aimCanvas.interactable = aimCanvas.blocksRaycasts = false;
-		quater.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, y, 0), ForceMode.Impulse);
+		quater.GetComponent<Rigidbody> ().AddForce (new Vector3 (0, bouncePower, 0), ForceMode.Impulse);
 		loadingBar.GetComponent<Image> ().fillAmount = 0;
-
 	}
 
 	public void OnPointerUp (PointerEventData eventData)
@@ -130,7 +124,6 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 
 	float PowerX (Vector2 currentPos)
 	{
-		// var t = endTime - startTime;
 		var x = Mathf.Abs (currentPos.x - aimStartPosition.x);
 		var power = 1f;
 		if (currentPos.x < aimStartPosition.x)
@@ -147,20 +140,18 @@ public class Aim : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandl
 
 	float PowerY (Vector2 currentPos)
 	{
-		//var t = endTime - startTime;
 		var y = currentPos.y - aimStartPosition.y;
-		//var power = 1 - (Mathf.Abs(y) / t) / 150;
-		float power = -8.5f + (y / bouncePower);
-		if (power < -12.5f)
-			power = -12.5f;
-		Debug.Log (power);
+		float power = -8f + (y / bounceDamper);
+		if (power < -14f)
+			power = -14f;
+		bouncePower = power;
 		return power;
 	}
 
 
 	float Fill (Vector2 currentPos)
 	{
-		var f = (Mathf.Abs (PowerY (currentPos)) - 8.5f) * 26f;
+		var f = (Mathf.Abs (PowerY (currentPos)) - 8f) * 17f;
 		return f;
 	}
 
--- Assets/Scripts/Quater.cs
+++ Assets/Scripts/Quater.cs
@@ -4,41 +4,40 @@ public class Quater : MonoBehaviour
 {
 	Rigidbody RB;
 	public float x;
-	int numBounce;
+	bool firstBounce;
 
 	void OnEnable ()
 	{
 		RB = GetComponent<Rigidbody> ();
+		RB.maxAngularVelocity = 50f;
+		RB.freezeRotation = true;
 	}
 
 	void OnCollisionEnter (Collision collision)
 	{
-		numBounce++;
-		if (numBounce == 1)
+		if (firstBounce)
+			return;
+		firstBounce = true;
+		var speed = RB.velocity;
+		var middle = 0 + Mathf.Abs (transform.position.x);
+		RB.freezeRotation = false;
+		RB.AddTorque (10, 0, 0);
+		RB.AddForce (new Vector3 (0, 0, 1), ForceMode.Impulse);
+		if (middle < 0.2f)
 		{
-			var speed = RB.velocity;
-			var middle = 0 + Mathf.Abs (transform.position.x);
-			if (middle < 0.2f)
-			{
-				RB.velocity = new Vector3 (0, speed.y, speed.z);
-			}
-			else if (transform.position.x >= 0.2f)
-			{
-				RB.velocity = new Vector3 (0.4f, speed.y, speed.z);
-				RB.AddForce (new Vector3 (x, 0, 0), ForceMode.Impulse);
-			}
-			else if (transform.position.x <= -0.2f)
-			{
-				RB.velocity = new Vector3 (-0.4f, speed.y, speed.z);
-				RB.AddForce (new Vector3 (x, 0, 0), ForceMode.Impulse);
-			}
-
-			if (QuaterBack.FirstBounce != null)
-			{
-				QuaterBack.FirstBounce ();
-				return;
-			}
-			
+			RB.velocity = new Vector3 (0, speed.y, speed.z);
 		}
+		else if (transform.position.x >= 0.2f)
+		{
+			RB.velocity = new Vector3 (0.4f, speed.y, speed.z);
+			RB.AddForce (new Vector3 (x, 0, 0), ForceMode.Impulse);
+		}
+		else if (transform.position.x <= -0.2f)
+		{
+			RB.velocity = new Vector3 (-0.4f, speed.y, speed.z);
+			RB.AddForce (new Vector3 (x, 0, 0), ForceMode.Impulse);
+		}
+		if (QuaterBack.FirstBounce != null)
+			QuaterBack.FirstBounce ();
 	}
 }
